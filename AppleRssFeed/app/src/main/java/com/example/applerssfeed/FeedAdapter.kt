fffad9f132ed4400be7b6b296896e977a7051ab8
= <anamika@nagarro.com>
2020-07-23 16:39:35 +0530

    menu items added

---
+++ AppleRssFeed/app/src/main/java/com/example/applerssfeed/FeedAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.applerssfeed
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.applerssfeed.Model.FeedEntry
+
+
+
+class ViewHolder(v:View){
+    val tvName = v.findViewById<TextView>(R.id.tvName)
+    val tvArtist = v.findViewById<TextView>(R.id.tvArtist)
+    val imageView = v.findViewById<ImageView>(R.id.imageView)
+}
+
+class FeedAdapter(context: Context, private val resource :Int, private val collection:List<FeedEntry>):ArrayAdapter<FeedEntry>(context,resource,collection) {
+    private val TAG = "FeedAdapter"
+    //inflate the xml resource to create the view
+    //we will be saving the view in inflator rather than fetching it again and again
+
+
+    private val inflator = LayoutInflater.from(context)
+    override fun getCount(): Int {
+        Log.d(TAG, "getCount")
+        return collection.size
+
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        Log.d(TAG, "getView")
+
+        val view: View
+        val viewHolder:ViewHolder
+// if view is already present and saved to view holder , we will reuse it instea of inflationa new one eveytime.
+        if(convertView == null){
+            view = inflator.inflate(resource,parent,false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        }else{
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+//         lets use viewholder pattern to save the view instead of finding it again and again .
+//        val tvName = view.findViewById<TextView>(R.id.tvName)
+//        val tvArtist = view.findViewById<TextView>(R.id.tvArtist)
+//        val Image = view.findViewById<ImageView>(R.id.imageView)
+
+        val currentApp = collection[position]
+
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        Glide
+            .with(context)
+            .load(currentApp.imageURL)
+            .into(viewHolder.imageView)
+
+
+        return  view
+    }
+}
--- AppleRssFeed/app/src/main/java/com/example/applerssfeed/MainActivity.kt
+++ AppleRssFeed/app/src/main/java/com/example/applerssfeed/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,11 +17,43 @@ import java.net.URL
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    //make sure to add lazy initlialisation . because we are referring to view before setContentLayout
+
+    private val downloader by lazy { Downloader(this, listView_Feed) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val download = Downloader(this, listView_Feed)
-        download.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml")
+
+        downloader.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=200/xml")
+    }
+    private fun downloadURL(feedUrl:String){
+        downloader.execute(feedUrl)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedURL: String
+        when(item.itemId){
+            R.id.mnufree -> {
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml"
+            }
+            R.id.mnupaid -> {
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=25/xml"
+            }
+            R.id.mnusongs-> {
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml"
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        downloadURL(feedURL)
+        return true
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        downloader.cancel(true)
     }
 
 
@@ -32,36 +66,44 @@ class MainActivity : AppCompatActivity() {
 
 
     companion object {
-        class Downloader(context:Context ,  list:ListView) : AsyncTask<String, Void, String>() {
+        private class Downloader(context: Context, list: ListView) :
+            AsyncTask<String, Void, String>() {
             private val TAG = "Downloader"
 
             /* Strong reference is being created . this leaks a context object.
             var propContext:Context = context
             var propListView:ListView = list*/
 
-            var propContext:Context by Delegates.notNull()
-            var propListView:ListView by Delegates.notNull()
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
 
-            init{
+            init {
                 propListView = list
                 propContext = context
             }
 
             override fun doInBackground(vararg url: String?): String {
-                Log.d(TAG, "doInBackground starts with: ${url.firstOrNull()?:""}")
-                val rssFeed = downloadXML(url.firstOrNull()?:"")
+                Log.d(TAG, "doInBackground starts with: ${url.firstOrNull() ?: ""}")
+                val rssFeed = downloadXML(url.firstOrNull() ?: "")
                 return rssFeed
             }
 
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-               Log.d(TAG, "onPostExecute")
+                Log.d(TAG, "onPostExecute")
                 /*observation : It is mandatory to create a object and access the list through that only.*/
-               var parseModule =  Parser()
+                var parseModule = Parser()
                 parseModule.parse(result)
 
-                val arrayAdaptor =  ArrayAdapter<FeedEntry>(propContext,R.layout.list_item,parseModule.getListOfFeeds())
-                propListView.adapter = arrayAdaptor
+//                val arrayAdaptor = ArrayAdapter<FeedEntry>(
+//                    propContext,
+//                    R.layout.list_item,
+//                    parseModule.getListOfFeeds()
+//                )
+//                propListView.adapter = arrayAdaptor
+                /*Using Custom Adapter*/
+                val feedAdapter = FeedAdapter(propContext,R.layout.list_record,parseModule.application)
+                propListView.adapter = feedAdapter
             }
 
             //            fun downloadXML(url: String): String {
@@ -118,7 +160,7 @@ class MainActivity : AppCompatActivity() {
 //        }
 //    }
             private fun downloadXML(url: String): String {
-                    return URL(url).readText()
+                return URL(url).readText()
             }
         }
     }
