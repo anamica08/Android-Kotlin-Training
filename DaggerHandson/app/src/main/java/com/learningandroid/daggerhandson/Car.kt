a192d4f54b3def679dadffb459c890968373289d
= <anamika@nagarro.com>
2020-08-13 00:29:20 +0530

    Constructor and method injection implemented

--- DaggerHandson/app/src/main/java/com/learningandroid/daggerhandson/Car.kt
+++ DaggerHandson/app/src/main/java/com/learningandroid/daggerhandson/Car.kt
@@ -1,2 +1,19 @@
 package com.learningandroid.daggerhandson
 
+import android.util.Log
+import javax.inject.Inject
+
+//constructor injection
+class Car @Inject constructor(private val wheel: Wheel, private val engine: Engine) {
+    private val TAG = "Car"
+
+    //Method Injection
+    @Inject
+    fun enableRemote(remote:Remote){
+        remote.setListener(this)
+    }
+
+    fun drive(){
+        Log.d(TAG, "drive:  Driving................ ")
+    }
+}
